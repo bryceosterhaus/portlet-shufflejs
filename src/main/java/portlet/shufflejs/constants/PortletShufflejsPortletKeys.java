29b9d0bfe2819e739be2ddc8604178e8c4d03957
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-11-25 13:00:40 -0800

    initialize

---
+++ src/main/java/portlet/shufflejs/constants/PortletShufflejsPortletKeys.java
@@ -0,0 +1,11 @@
+package portlet.shufflejs.constants;
+
+/**
+ * @author bryceosterhaus
+ */
+public class PortletShufflejsPortletKeys {
+
+	public static final String PORTLETSHUFFLEJS =
+		"portlet_shufflejs_PortletShufflejsPortlet";
+
+}
---
+++ src/main/java/portlet/shufflejs/portlet/PortletShufflejsPortlet.java
@@ -0,0 +1,30 @@
+package portlet.shufflejs.portlet;
+
+import portlet.shufflejs.constants.PortletShufflejsPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author bryceosterhaus
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=PortletShufflejs",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PortletShufflejsPortletKeys.PORTLETSHUFFLEJS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class PortletShufflejsPortlet extends MVCPortlet {
+}
